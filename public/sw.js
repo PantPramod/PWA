5ce9f6461bced7b9801e1786345584b6aba7f042
Pramod Pant <pramodpant100@gmail.com>
2023-03-30 15:11:53 +0530

    first commit

---
+++ public/sw.js
@@ -0,0 +1,33 @@
+let cacheData = "appV1"
+
+
+this.addEventListener("install", (event) => {
+  event.waitUntill(
+    caches.open(cacheData).then((cache) => {
+      cache.addAll([
+        'static/js/main.chunk.js',
+        'static/js/0.chunk.js',
+        'static/js/bundle.js',
+        '/index.html',
+        '/'
+      ])
+    })
+  )
+})
+
+
+this.addEventListener("fetch", (event) => {
+  if (!navigator.onLine) {
+    event.respondWith(
+      caches.match(event.request).then((resp) => {
+        if (resp) {
+          return resp
+        }
+        let requestUrl= event.request.clone();
+        fetch(requestUrl)
+        return requestUrl
+      })
+    )
+  }
+})
+
